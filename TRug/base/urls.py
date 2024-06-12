ae259a85ae699a6f3519c882cb3dd3fa27ec9f48
TRIFK <32969399+TRIFK@users.noreply.github.com>
2024-06-12 23:47:30 +0300

    added db

--- TRug/base/urls.py
+++ TRug/base/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-from django.urls import path
-from . import views
 
 
 from django.urls import path
@@ -12,4 +10,10 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('auth/', views.auth, name='auth'),
     path('home', views.home, name='home'),
+    path('logout/', views.logout_view, name='logout'),
+    path('products/', views.products, name='products'),
+    path('placement/', views.placement, name='placement'),
+    path('supplies/', views.supplies, name='supplies'),
+    path('shipment/', views.shipment, name='shipment'),
+    path('orders/', views.orders, name='orders'),
 ]
--- TRug/base/views.py
+++ TRug/base/views.py
@@ -1,22 +1,31 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User, Group
 from .forms import RegistrationForm
-
-
+from django.contrib.auth.forms import AuthenticationForm
 
 def auth(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return render(request, 'auth.html', {'form': form, 'error': 'Неверные учетные данные'})
         else:
-            # Добавить сообщение об ошибке или обработку
-            return render(request, 'auth.html', {'error': 'Неверные учетные данные'})
-    return render(request, 'auth.html')
+            return render(request, 'auth.html', {'form': form, 'error': 'Неверные учетные данные'})
+    else:
+        form = AuthenticationForm()
+    return render(request, 'auth.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('welcome')
 
 
 def welcome(request):
@@ -58,5 +67,20 @@ def register(request):
 
 def home(request):
     return render(request, 'home.html')
+
+def products(request):
+    return render(request, 'products.html')
+
+def placement(request):
+    return render(request, 'placement.html')
+
+def supplies(request):
+    return render(request, 'supplies.html')
+
+def shipment(request):
+    return render(request, 'shipment.html')
+
+def orders(request):
+    return render(request, 'orders.html')
 
 
